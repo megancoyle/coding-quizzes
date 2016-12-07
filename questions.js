7a4a68f86a3ee759e1b9960b54d3b340902c63c6
Megan Coyle <meg@mcoyle.com>
2016-12-07 09:24:19 -0500

    Fix broken string

--- questions.js
+++ questions.js
@@ -48,7 +48,7 @@ var questions = {
     "What are the pros and cons of functional programming vs object-oriented programming?",
     "When is classical inheritance an appropriate choice?",
     "When is prototypal inheritance an appropriate choice?",
-    "What does “favor object composition over class inheritance” mean?",
+    "What does favor object composition over class inheritance mean?",
     "What are two-way data binding and one-way data flow, and how are they different?",
     "What are the pros and cons of monolithic vs microservice architectures?",
     "What is asynchronous programming, and why is it important in JavaScript?",
